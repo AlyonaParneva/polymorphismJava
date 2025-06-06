8b1273693ad9f1cd688ac5eecaaa98247e681054
AlyonaParneva <parneva.aa@gmail.com>
2025-06-06 16:26:48 +0300

    task1

---
+++ polymorphism/src/Task1/Main1.java
@@ -0,0 +1,11 @@
+package Task1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Point point2D = new Point(3, 4);
+        System.out.println("2D Point: " + point2D);
+
+        Point3D point3D = new Point3D(3, 4, 5);
+        System.out.println("3D Point: " + point3D);
+    }
+}
